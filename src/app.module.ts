3fb7676ad2f4cf7f60d4ca151b86670d168dd2c6
thgr8ganzi <q33asq33as@gmail.com>
2023-06-19 00:04:03 +0900

    2023-06-19

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
-import { UsersModule } from './users/users.module';
 import { CatsService } from './cats/cats.service';
 
 @Module({
-  imports: [CatsModule, UsersModule],
+  imports: [CatsModule],
   controllers: [AppController],
   providers: [AppService,CatsService],
 })
