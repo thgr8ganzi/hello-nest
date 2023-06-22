7564d7daf6fb2a42b5a7018e6e90293c78338ae7
leejisoo <leejisoo@atnsgroup.com>
2023-06-22 09:23:32 +0900

    2023-06-22 swagger cors

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -4,6 +4,8 @@ import { HttpExceptionFilter } from 'src/common/exceptions/http-exception.filter
 import { SuccessInterceptor } from 'src/common/interceptors/success.interceptor';
 import { CatsService } from './cats.service';
 import { CatRequestDto } from './dto/cats.request.dto';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ReadOnlyCatDto } from './dto/cay.dto';
 
 @Controller('cats')
 @UseInterceptors(SuccessInterceptor)
@@ -11,26 +13,38 @@ import { CatRequestDto } from './dto/cats.request.dto';
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
+  @ApiOperation({ summary: '현재 고양이 가져오기' })
   @Get()
   getCurrentCat() {
     return 'current cat';
   }
 
+  @ApiResponse({
+    status: 500,
+    description: 'Server Error...',
+  })
+  @ApiResponse({
+    status: 200,
+    description: '성공!',
+    type: ReadOnlyCatDto,
+  })
+  @ApiOperation({ summary: '회원가입' })
   @Post()
   async signUp(@Body() body: CatRequestDto) {
     return await this.catsService.signUp(body);
   }
 
+  @ApiOperation({ summary: '로그인' })
   @Post('login')
   logIn() {
     return 'login';
   }
-
+  @ApiOperation({ summary: '로그아웃' })
   @Post('logout')
   logOut() {
     return 'logout';
   }
-
+  @ApiOperation({ summary: '이미지 업로드' })
   @Post('upload/cats')
   uploadCatImg() {
     return 'uploadImg';
